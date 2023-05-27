2aa60d500a41262df8bdd1c052bdb151ac9ab94f
Aditya <adityaraj2019.sahu@gmail.com>
2023-05-27 10:40:08 +0530

    merge intervals

---
+++ Arrays-II/merge_intervals.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> ans;
+    sort(intervals.begin(), intervals.end());
+
+    for(int i = 0; i < intervals.size(); i++) {
+        int start = intervals[i][0], end = intervals[i][1];
+        while(i < intervals.size() && intervals[i][0] <= e) {
+            end = max(end, intervals[i][1]);
+            i++;
+        } 
+        ans.push_back({start, end});
+    }  
+    return ans;
+}
