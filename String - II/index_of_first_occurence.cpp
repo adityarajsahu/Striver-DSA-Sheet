8c6ce0af7ea2fa4ac72cc1f005ab8942ceb4e6c2
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-06-03 15:08:21 +0530

    find index of first occurence of substring in a string

---
+++ String - II/index_of_first_occurence.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int strStr(string haystack, string needle) {
+    if(haystack.find(needle) != string::npos) {
+        return found;
+    }
+    return -1;
+}
