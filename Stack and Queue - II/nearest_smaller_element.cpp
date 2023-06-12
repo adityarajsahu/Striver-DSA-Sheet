cda12b5f9a8fcaa70366e6eff75fed041a23d0f1
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-06-12 12:51:20 +0530

    nearest smaller element

---
+++ Stack and Queue - II/nearest_smaller_element.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int prevSmaller(vector<int> &A) {
+    unordered_map<int, int> m;
+    stack<int> s;
+
+    for(int i = A.size() - 1; i >= 0; i--) {
+        while(!s.empty() && s.top() > num) {
+            m[s.top()] = num;
+            s.pop();
+        }
+        s.push(num);
+    }
+
+    vector<int> ans;
+    for(auto num : A) {
+        if(m.find(num) != m.end()) {
+            ans.push_back(m[num]);
+        } else {
+            ans.push_back(-1);
+        }
+    }
+    return ans;
+}
