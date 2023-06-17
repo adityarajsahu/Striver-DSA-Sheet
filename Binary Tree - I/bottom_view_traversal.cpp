d47bbfe3d0f65d7501cf5e24c83caf4a622868e0
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-06-17 23:59:20 +0530

    bottom view traversal

---
+++ Binary Tree - I/bottom_view_traversal.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <int> bottomView(Node *root) {
+    vector<int> ans;
+    if(root == NULL) {
+        return ans;
+    }
+    
+    queue<pair<Node*, int>> q;
+    map<int, int> m;
+    
+    q.push({root, 0});
+    
+    while(!q.empty()) {
+        auto p = q.front();
+        q.pop();
+        
+        Node* node = p.first;
+        int level = p.second;
+        
+        m[level] = node->data;
+        
+        if(node->left) {
+            q.push({node->left, level - 1});
+        }
+        
+        if(node->right) {
+            q.push({node->right, level + 1});
+        }
+    }
+    
+    for(auto x : m) {
+        ans.push_back(x.second);
+    }
+    
+    return ans;
+}
