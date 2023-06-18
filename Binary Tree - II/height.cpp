13c73777a93136943ab4272f981483b022b49f7f
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-06-18 18:40:19 +0530

    height of a binary tree

---
+++ Binary Tree - II/height.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxDepth(TreeNode* root) {
+    if(root == NULL) {
+        return 0;
+    }
+    
+    int lh = maxDepth(root->left);
+    int rh = maxDepth(root->right);
+    
+    return max(lh, rh) + 1;
+}
