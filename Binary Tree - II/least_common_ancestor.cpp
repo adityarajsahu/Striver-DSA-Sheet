11dd642517cef0d55aae5d56fcc1b88f22a11f11
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-06-19 07:21:11 +0530

    least common ancestor

---
+++ Binary Tree - II/least_common_ancestor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root == NULL || root == p || root == q) {
+        return root;
+    }
+    
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    
+    if(!left) {
+        return right;
+    }
+    
+    if(!right) {
+        return left;
+    }
+    return root;
+}
