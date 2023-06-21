262f3c948f04590f332d86a19a4de00553113eaa
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-06-21 11:10:46 +0530

    mirror of a binary tree

---
+++ Binary Tree - III/mirror_binary_tree.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void mirror(Node* node) {
+    Node* temp;
+    if(node == NULL)
+    {
+        return;
+    }
+    mirror(node->left);
+    mirror(node->right);
+     
+    temp = node->left;
+    node->left = node->right;
+    node->right = temp;
+}
