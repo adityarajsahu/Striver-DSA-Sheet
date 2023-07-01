47f526a7c3ef3c0536143ec2485d8c7b806c4268
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-07-01 13:51:53 +0530

    Search in sorted rotated array

---
+++ Binary Search/Find_element_that_appears_once.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNonDuplicate(vector<int>& nums) {
+    int ans = nums[0];
+    for(int i = 1; i < nums.size(); i++) {
+        ans ^= nums[i];
+    }
+    return ans;
+}
