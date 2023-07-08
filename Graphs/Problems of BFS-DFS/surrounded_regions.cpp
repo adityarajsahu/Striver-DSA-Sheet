489c932b7274dc83de9e00cef0d22678427a3eed
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-07-08 18:03:22 +0530

    Surrounded regions

---
+++ Graphs/Problems of BFS-DFS/surrounded_regions.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int m, int n, int row, int col, vector<vector<char>>& board, vector<vector<int>> &vis, int delrow[], int delcol[]) {
+    vis[row][col] = 1;
+    
+    for(int i = 0; i < 4; i++){
+        int nrow = row + delrow[i];
+        int ncol = col + delcol[i];
+        
+        if(nrow >= 0 && ncol >= 0 && nrow < m && ncol < n && board[nrow][ncol] == 'O' && vis[nrow][ncol] == 0)
+            dfs(m, n, nrow, ncol, board, vis, delrow, delcol);
+    }
+}
+
+void solve(vector<vector<char>>& board) {
+    int m = board.size();
+    int n = board[0].size();
+    vector<vector<int>> vis(m, vector<int>(n, 0));
+    int delrow[] = {-1, 1, 0, 0};
+    int delcol[] = {0, 0, -1, 1};
+    
+    for(int i = 0; i < m; i++){
+        if(board[i][0] == 'O' && vis[i][0] == 0)
+            dfs(m, n, i, 0, board, vis, delrow, delcol);
+        
+            if(board[i][n-1] == 'O' && vis[i][n-1] == 0)
+            dfs(m, n, i, n - 1, board, vis, delrow, delcol);
+        
+    }
+    
+    for(int j = 0; j < n; j++){
+        if(board[0][j] == 'O' && vis[0][j] == 0)
+            dfs(m, n, 0, j, board, vis, delrow, delcol);
+        
+            if(board[m - 1][j] == 'O' && vis[m-1][j] == 0)
+            dfs(m, n, m-1, j, board, vis, delrow, delcol);
+    }
+    
+    for(int i = 0; i < m; i++){
+        for(int j = 0;j < n; j++){
+            if(board[i][j] == 'O' && vis[i][j] == 0)
+                board[i][j] = 'X';
+        }
+    }
+}
