49b9b7604059a5013493c54ffcb93572c6527b63
Adityaraj Sahu <54609034+adityarajsahu@users.noreply.github.com>
2023-07-10 23:14:25 +0530

    Word Ladder - I

---
+++ Graphs/Problems of BFS-DFS/word_ladder_one.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    queue<pair<string, int>> q;
+    q.push({beginWord, 1});
+    
+    unordered_set<string> st(wordList.begin(), wordList.end());
+    st.erase(beginWord);
+    
+    while(!q.empty()) {
+        string word = q.front().first;
+        int count = q.front().second;
+        q.pop();
+        
+        if(word == endWord) {
+            return count;
+        }
+        
+        for(int i = 0; i < word.size(); i++) {
+            char original = word[i];
+            
+            for(char ch = 'a'; ch <= 'z'; ch++) {
+                word[i] = ch;
+                if(st.find(word) != st.end()) {
+                    st.erase(word);
+                    q.push({word, count + 1});
+                }
+            }
+            word[i] = original;
+        }
+    }
+    return 0;
+}
